775b3f413f23dfe78fcb261ab64708f9e3f96aa6
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2024-10-02 21:30:26 +0200

    Add out folder to save the generated image

--- src/run.py
+++ src/run.py
@@ -1,17 +1,27 @@
 from mflux.flux.flux import Flux1
 from mflux.config.config import Config
+from pathlib import Path
 
-flux = Flux1.from_alias(
-    alias="schnell",
-    quantize=8,
-)
 
-prompt = "A cat holding a sign that says hello world"
+def main():
+    outPath = Path(__file__).parent.parent / "out"
+    outPath.mkdir(parents=True, exist_ok=True)
 
-image = flux.generate_image(
-    prompt=prompt,
-    seed=0,
-    config=Config(num_inference_steps=4),
-)
+    flux = Flux1.from_alias(
+        alias="schnell",
+        quantize=8,
+    )
 
-image.save(path="flux-schnell.png")
+    prompt = "A cat holding a sign that says hello world"
+
+    image = flux.generate_image(
+        prompt=prompt,
+        seed=0,
+        config=Config(num_inference_steps=4),
+    )
+
+    image.save(outPath / "flux-schnell.png")
+
+
+if __name__ == "__main__":
+    main()
